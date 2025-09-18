14061b74be1ce6a27229f5a96eb45cdf6a08ca8a
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-09-17 19:57:13 -0500

    test(matrix): add test case for rotateMatrixCounterClockwise function

--- src/app/_lib/matrix.test.ts
+++ src/app/_lib/matrix.test.ts
@@ -1,3 +1,18 @@
-import { describe } from "vitest";
+import { describe, expect, it } from "vitest";
+import { rotateMatrixCounterClockwise } from "./matrix";
 
-describe("rotateMatrixClockwise", () => {});
+describe("rotateMatrixClockwise", () => {
+    it("debe rotar correctamente una matriz de 2x2", () => {
+        const input = [
+            [1, 2],
+            [3, 4],
+        ];
+        const expected = [
+            [2, 4],
+            [1, 3],
+        ];
+
+        const result = rotateMatrixCounterClockwise(input);
+        expect(result).toEqual(expected);
+    });
+});
